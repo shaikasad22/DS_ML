85c53e4d6f03929c682eb7a0a833895c44429c0a
Shaik Asad <149304716+shaikasad22@users.noreply.github.com>
2025-10-26 04:23:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
-    '''
+    ''' 
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
