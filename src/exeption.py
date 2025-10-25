c78ba5d334833ef3b72c61e441420ede17330dcf
Shaik Asad <149304716+shaikasad22@users.noreply.github.com>
2025-10-26 04:24:23 +0530

    Update exeption.py

--- src/exeption.py
+++ src/exeption.py
@@ -4,7 +4,7 @@ from src.logger import logging
 def error_message_detail(error, error_detail: sys):
     """
     Generates a detailed error message including filename, line number, and error text.
-    """
+    """ 
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in python script [{0}] line number [{1}] error message [{2}]".format(
